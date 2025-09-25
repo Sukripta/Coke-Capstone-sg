9368f8aa724def3c5acc2154f6bd36749dc039c3
Sukripta <ghosh.sukripto007@gmail.com>
2025-09-26 02:09:29 +0530

    implemented update book functionality

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -37,8 +37,49 @@ exports.findAll = async (request,response) => {
     const data = await Book.find({});
     response.status(200).send(data);
   } catch (err) {
+    console.error("Error: ", err);
     response.status(500).send({
-      message: err.message || "Error occurred while retrieving books."
+      message: "Error occurred while retrieving books."
     });
   }
 };
+
+exports.update = async (request, response) => {
+  const { id } = request.params;
+  const { title, author, genre, year, description } = request.body;
+
+  try {
+    if (!id || !(title || author || genre || year || description)) {
+      return response.status(400).send({ 
+        message: "Content cannot be empty and an ID must be provided"
+      });
+    }
+
+    let newTitle = title ? title.toUpperCase() : undefined;
+    if (newTitle) {
+      const isDuplicateTitle = await checkDuplicateName(newTitle);
+      if (isDuplicateTitle.length) {
+        return response.status(409).send({ message: "Title already exists." });
+      }
+    }
+
+    const newBook = {};
+    newBook.title = newTitle;
+    newBook.author = author;
+    newBook.genre = genre;
+    newBook.year = year;
+    newBook.description = description;
+
+    // Find the book by ID
+    const updatedBook = await Book.findByIdAndUpdate(id, newBook, { new: true, runValidators: true });
+    if (!updatedBook) {
+      return response.status(404).send({ message: `Cannot update book with id=${id}. Book not found.` });
+    } else {
+      response.status(200).send(updatedBook);
+    }
+  } catch (err) {
+    console.error("Error during update operation:", err);
+    response.status(500).send({ message: "Error updating book" });
+  }
+};
+
--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -4,4 +4,5 @@ module.exports = function(app) {
 
     app.post("/api/books", controller.add); //public route
     app.get("/api/books", controller.findAll);
+    app.put("/api/books/:id", controller.update);
 }
